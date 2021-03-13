3003902cce358209d800b06c80fca03ac98ad8b5
sabbircsx <45328851+sabbircs06@users.noreply.github.com>
2021-03-13 17:36:32 +0600

    Update main.js
    JS Object

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
-let nameArr = ["A", "B", "C", "D"];
 
-/* nameArr.push("AAA");
+/* let nameArr = ["A", "B", "C", "D", "E", "F", 9, 10];
+
+nameArr.push("AAA");
 nameArr.push("ZZZ");
 
 nameArr.pop();
 nameArr.shift();
 nameArr.unshift("SAIF");
-console.log(nameArr.indexOf("Rimon")); */
+console.log(nameArr.indexOf("Rimon"));
 
-/* for (let i = 0; i < nameArr.length; i++) {
+for (let i = 0; i < nameArr.length; i++) {
   console.log(nameArr[i]);
-} */
+}
 
-nameArr.splice(0,2);
-console.log(nameArr);
+nameArr.splice(1, 3);
+console.log(nameArr); */
+
+// ! OBJECT
+
+var info = {
+  fName: 'SABBIR',
+  sName: 'AHMED',
+  age: 29,
+  myFunc: function () {
+    console.log("Printed inside myFunc");
+  }
+}
+
+console.log(info.age);
+console.log(info['fName']);
+info.myFunc();
