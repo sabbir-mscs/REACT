5e7991502cb88e8c04ef36f876d6222c08776e6a
sabbircsx <45328851+sabbircs06@users.noreply.github.com>
2021-03-13 14:48:34 +0600

    Commit-13th_March
    Uploading HTML Boilerplate

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let nameArr = ["A", "B", "C", "D"];
+
+/* nameArr.push("AAA");
+nameArr.push("ZZZ");
+
+nameArr.pop();
+nameArr.shift();
+nameArr.unshift("SAIF");
+console.log(nameArr.indexOf("Rimon")); */
+
+/* for (let i = 0; i < nameArr.length; i++) {
+  console.log(nameArr[i]);
+} */
+
+nameArr.splice(0,2);
+console.log(nameArr);
