daa33d1f45692befc00dde46a77554875327b6ad
SABBIR AHMED <sabbir.mscs@gmail.com>
2022-02-08 10:27:02 +0600

    Update advance-react

--- advance-react/src/pages/NewMeetups.js
+++ advance-react/src/pages/NewMeetups.js
@@ -1,10 +1,13 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function Favourites() {
+  function addMeetupHandler(meetupData) {
+    //https://advance-react-back-default-rtdb.firebaseio.com/
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
