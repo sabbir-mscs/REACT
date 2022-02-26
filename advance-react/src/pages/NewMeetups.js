419c8328ddfb3e3040955c17248a75bd657b3c58
SABBIR AHMED <sabbir.mscs@gmail.com>
2022-02-26 16:03:51 +0600

    Working on AllMeetups

--- advance-react/src/pages/NewMeetups.js
+++ advance-react/src/pages/NewMeetups.js
@@ -6,9 +6,6 @@ function NewMeetupPage() {
 
   function addMeetupHandler(meetupData) {
     //https://advance-react-back-default-rtdb.firebaseio.com/
-<<<<<<< HEAD
-    fetch("https://advance-react-back-default-rtdb.firebaseio.com/meetups");
-=======
     fetch(
       "https://advance-react-back-default-rtdb.firebaseio.com/meetups.json",
       {
@@ -21,7 +18,6 @@ function NewMeetupPage() {
     ).then(() => {
       history.replace("/");
     });
->>>>>>> 201b4236c6ecec43f22632be79c20721953fb9ea
   }
   return (
     <section>
